b6148fb768ef8330276ed2d801cfd6c3c0689f0a
Pedro Santana <pedro.ferraz01@gmail.com>
2024-03-15 09:01:01 -0300

    feat: axios to fetch data

---
+++ frontend/src/utils/axios.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const apiInstance = axios.create({
+  baseURL: process.env.RUBY_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default apiInstance;
