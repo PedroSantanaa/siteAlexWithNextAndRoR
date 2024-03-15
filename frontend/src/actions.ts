396c7b7ac13fdda170337b6a10ee453b0a4c0fb0
Pedro Santana <pedro.ferraz01@gmail.com>
2024-03-15 09:11:59 -0300

    fix: return null if no session

--- frontend/src/actions.ts
+++ frontend/src/actions.ts
@@ -6,6 +6,10 @@ import apiInstance from "./utils/axios"
 
 export const getSession =  async () => {
   const session = await getIronSession(cookies(), sessionOptions)
+  if (Object.keys(session).length === 0) {
+    return null
+  }
+  
   const user = apiInstance.get('/current_user',{
     headers: {
       'Authorization': `Bearer ${session}`
