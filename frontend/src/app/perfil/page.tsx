2bb0053819ecdc2ef0b59b48096ebdd0c06e18d6
Pedro Santana <pedro.ferraz01@gmail.com>
2024-05-02 08:51:11 -0300

    feat: finish profile page

--- frontend/src/app/perfil/page.tsx
+++ frontend/src/app/perfil/page.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 import { Container } from '../styled-components/GeneralStyle'
 import { EditProfileButton, ProfileContainer, ProfileDetailsCategorySubText, ProfileDetailsCategoryText, ProfileDetailsMain, ProfileDetailsMainSubtitle,  ProfileDetailsMainText,  ProfileDetailsText,  ProfileGridContainer,  ProfileHeader,ProfileHeaderLine,ProfileHeaderSubtitle,ProfileHeaderTitle,ProfileInfo, ProfileInfoSubtitle} from '../styled-components/ProfileStyled'
 import { useFetchCurrentUser } from '@/hooks/useFetchCurrentUser'
+import { useFetchCompany } from '@/hooks/useFetchCompany'
 
 const Profile =  () => {
   const { currentUser:currentUser, loading, error } = useFetchCurrentUser();
+  const {company:company} = useFetchCompany(currentUser?.id || 0);
   return (
     <Container>
       <ProfileContainer>
@@ -39,21 +41,21 @@ const Profile =  () => {
           </ProfileGridContainer>
           <ProfileDetailsText>DADOS DA EMPRESA</ProfileDetailsText>
           <ProfileGridContainer>
-            <ProfileDetailsCategoryText>CNPJ: <ProfileDetailsCategorySubText>-</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
-            <ProfileDetailsCategoryText>Nome Fantasia: <ProfileDetailsCategorySubText>0</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
-            <ProfileDetailsCategoryText>Razão Social: <ProfileDetailsCategorySubText>056......</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
-            <ProfileDetailsCategoryText>Registro Estadual: <ProfileDetailsCategorySubText>056......</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
-            <ProfileDetailsCategoryText>Registro Municipal: <ProfileDetailsCategorySubText>056......</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
+            <ProfileDetailsCategoryText>CNPJ: <ProfileDetailsCategorySubText>{company?.cnpj}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
+            <ProfileDetailsCategoryText>Nome Fantasia: <ProfileDetailsCategorySubText>{company?.nome_fantasia}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
+            <ProfileDetailsCategoryText>Razão Social: <ProfileDetailsCategorySubText>{company?.razao_social}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
+            <ProfileDetailsCategoryText>Registro Estadual: <ProfileDetailsCategorySubText>{company?.registro_estadual}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
+            <ProfileDetailsCategoryText>Registro Municipal: <ProfileDetailsCategorySubText>{company?.registro_municipal}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
           </ProfileGridContainer>
           <ProfileDetailsText>ENDEREÇO DA EMPRESA</ProfileDetailsText>
           <ProfileGridContainer>
-            <ProfileDetailsCategoryText>CEP: <ProfileDetailsCategorySubText>{currentUser?.cep}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
-            <ProfileDetailsCategoryText>Estado: <ProfileDetailsCategorySubText>{currentUser?.estado}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
-            <ProfileDetailsCategoryText>Rua: <ProfileDetailsCategorySubText>{currentUser?.rua}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
-            <ProfileDetailsCategoryText>Bairro: <ProfileDetailsCategorySubText>{currentUser?.bairro}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
-            <ProfileDetailsCategoryText>Cidade: <ProfileDetailsCategorySubText>{currentUser?.cidade}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
-            <ProfileDetailsCategoryText>Nº: <ProfileDetailsCategorySubText>{currentUser?.numero}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
-            <ProfileDetailsCategoryText>Complemento: <ProfileDetailsCategorySubText>{currentUser?.complemento}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
+            <ProfileDetailsCategoryText>CEP: <ProfileDetailsCategorySubText>{company?.cep}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
+            <ProfileDetailsCategoryText>Estado: <ProfileDetailsCategorySubText>{company?.estado}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
+            <ProfileDetailsCategoryText>Rua: <ProfileDetailsCategorySubText>{company?.rua}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
+            <ProfileDetailsCategoryText>Bairro: <ProfileDetailsCategorySubText>{company?.bairro}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
+            <ProfileDetailsCategoryText>Cidade: <ProfileDetailsCategorySubText>{company?.cidade}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
+            <ProfileDetailsCategoryText>Nº: <ProfileDetailsCategorySubText>{company?.numero}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
+            <ProfileDetailsCategoryText>Complemento: <ProfileDetailsCategorySubText>{company?.complemento}</ProfileDetailsCategorySubText></ProfileDetailsCategoryText>
           </ProfileGridContainer>
           <ProfileDetailsText>CARGO NA EMPRESA</ProfileDetailsText>
           <ProfileGridContainer>
