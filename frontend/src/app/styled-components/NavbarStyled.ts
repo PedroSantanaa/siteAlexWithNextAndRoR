a96108b7c33bfdeab69a893150f3fd94c30db065
Pedro Santana <pedro.ferraz01@gmail.com>
2024-05-03 08:19:36 -0300

    refactor: change navbar justify content

--- frontend/src/app/styled-components/NavbarStyled.ts
+++ frontend/src/app/styled-components/NavbarStyled.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const NavBarContainer = styled.div`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   align-items: center;
   gap: 20px;
   background-color: white;
@@ -35,7 +36,8 @@ export const NavbarLinkStyled = styled.a<{ $isactive?: boolean; }>`
   text-decoration: none;
   padding: 10px 10px;
   text-align: center;
-  background-color: ${({ $isactive }) => $isactive ? '#e5e5e5' : 'transparent'};
+  background: ${({ $isactive }) => $isactive ? 'linear-gradient(to right, #fca311 1.5%, #e5e5e5 1.5%)' : 'transparent'};
+  transition: background 0.1s, transform 0.6s;
   &:hover {
     background: linear-gradient(to right, #fca311 10%, #e5e5e5 10%); background-size: 100% 100%; background-position: left bottom; background-repeat: no-repeat;
     transform: scale(1.2);
