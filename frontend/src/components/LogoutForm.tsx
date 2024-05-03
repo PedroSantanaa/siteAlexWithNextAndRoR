f09b2b7508139aa98b2b6c56733564e08b1e8968
Pedro Santana <pedro.ferraz01@gmail.com>
2024-05-03 08:19:51 -0300

    feat: add style to logout form

--- frontend/src/components/LogoutForm.tsx
+++ frontend/src/components/LogoutForm.tsx
@@ -1,12 +1,13 @@
 import { logout } from '@/actions'
+import { LogoutButton, LogoutContainer } from '@/app/styled-components/LogoutForm'
 import Image from 'next/image'
 import React from 'react'
 
 const LogoutForm = () => {
   return (
-    <form action={logout}>
-      <button><Image src='/logout.svg' alt='logout' width={30} height={30}/></button>
-    </form>
+    <LogoutContainer action={logout}>
+      <LogoutButton><Image src='/logout.svg' alt='logout' width={30} height={30}/></LogoutButton>
+    </LogoutContainer>
   )
 }
 
