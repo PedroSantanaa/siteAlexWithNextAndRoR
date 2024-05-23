7bdc53e8014f4eab281ab425ed280529f6f65aa6
Pedro Santana <pedro.ferraz01@gmail.com>
2024-05-23 08:49:59 -0300

    feat: new styles

--- frontend/src/app/styled-components/NewProject.ts
+++ frontend/src/app/styled-components/NewProject.ts
@@ -46,3 +46,111 @@ export const NewProjectDetailsText = styled.p`
   color: #374151;
   font-weight: bold;
 `;
+
+export const NewProjectSelectContainer = styled.div `
+  display: flex;
+  gap: 10px;
+`
+
+export const NewProjectSelect = styled.select `
+  width: 20%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+export const NewProjectInput = styled.input `
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+export const NewProjectTypeOfPersonButtonContainer = styled.div `
+  display: flex;
+  align-items: center;  
+  margin-bottom: 10px;
+`;
+
+export const NewProjectTypeOfPersonButton = styled.button<{ $isactive?: boolean; }>`
+  padding: 10px;
+  border: 1px solid #ccc;
+    background-color: ${({ $isactive }) => $isactive ? '#007bff' : 'transparent'};
+    color: ${({ $isactive }) => $isactive ? '#fff' : '#374151'};
+    border-radius: 4px;
+    cursor: pointer;
+`;
+
+export const NewProjectCPFOrCNPJContainer = styled.div `
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const NewProjectCPFOrCNPJInput = styled.input `
+  width: 30%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+export const NewProjectNomeorRazaoSocialInput = styled.input `
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+export const NewProjectDisjuntorContainer = styled.div `
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const NewProjectSelectDisjuntorType = styled.select `
+  width: 20%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+export const NewProjectSelectDisjuntorValue = styled.select `
+  width: 30%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`
+
+export const NewProjectLatitudeELongitudeContainer = styled.div `
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const NewProjectLatitudeInput = styled.input `
+  width: 30%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+export const NewProjectLongitudeInput = styled.input `
+  width: 30%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+export const NewProjectSelectTitularidade = styled.select `
+  width: 20%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+export const NewProjectSelectMaterial = styled.select `
+  width: 20%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
